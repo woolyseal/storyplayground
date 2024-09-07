a75622e9c214ac0e3650e7cccd2798faa6dd49ef
Janina <hard@werkbank.de>
2024-09-07 17:12:02 +0200

    Extend User Profil

--- storyplayground/settings.py
+++ storyplayground/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_ckeditor_5',
+    'users',
     'rpgs',
 
 ]
@@ -124,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Using own User Settings/Profile
+AUTH_USER_MODEL = "users.User"
