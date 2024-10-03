b8889b37b8b2ad9beff29bfb514d7e05abad917d
Janina <hard@werkbank.de>
2024-10-03 19:39:00 +0200

    First work on nav and css for header

--- rpgs/tests.py
+++ rpgs/tests.py
@@ -6,3 +6,13 @@ class RolePlayModelTest(TestCase):
     def test_roleplay_model_exists(self):
         roleplay = RolePlay.objects.count()
         self.assertGreaterEqual(roleplay, 0)
+
+
+class FrontEndViewTest(TestCase):
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_rpg_list(self):
+        response = self.client.get('/rpgs/')
+        self.assertEqual(response.status_code, 200)
--- rpgs/views.py
+++ rpgs/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
+
+class RPGListView(TemplateView):
+    template_name = 'rpg_list.html'
 
 def index(request):
     return HttpResponse("Hello, world. You're at the rpgs index.")
